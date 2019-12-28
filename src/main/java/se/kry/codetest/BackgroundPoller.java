8183377b6a220a47f8ebc344fab2a419157677f3
Anne-Laure Ducrocq <ducrocq.annelaure@gmail.com>
2019-12-28 18:10:57 +0100

    implement poller

--- src/main/java/se/kry/codetest/BackgroundPoller.java
+++ src/main/java/se/kry/codetest/BackgroundPoller.java
@@ -2,14 +2,45 @@ package se.kry.codetest;
 
 import io.vertx.core.Future;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
+import java.util.stream.Collectors;
 
 public class BackgroundPoller {
 
   public Future<List<String>> pollServices(Map<String, String> services) {
-    //TODO
-    return Future.failedFuture("TODO");
+    Future<List<String>> futures = Future.future();
+    List<String> results = services
+            .entrySet()
+            .stream()
+            .map(service -> getStatus(service.getKey()))
+            .collect(Collectors.toList());
+    futures.complete(results);
+    return futures;
+  }
+
+  private String getStatus(String url) {
+    int code = 0;
+    try {
+      URL siteURL = new URL(url);
+      HttpURLConnection connection = null;
+      connection = (HttpURLConnection) siteURL.openConnection();
+
+      connection.setRequestMethod("GET");
+      connection.setConnectTimeout(3000);
+      connection.connect();
+      code = connection.getResponseCode();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "FAIL";
+    }
+    if (code == 200) {
+      return "OK";
+    }
+    return "FAIL";
   }
 }
